0079b066eea44e5805008feb1f3de5f610c6c1b0
Marcelearn <jaworskimarcel@gmail.com>
2022-11-05 16:32:50 +0100

    movies card

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
 import './App.css';
 import {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 import Movies from './components/Movies';
 
 
-
 function App() {
-  const [data, setData] = useState();
+  const [data, setData] = useState([]);
   const [searchText, setSearchText] = useState("");
-  
-  console.log(searchText)
+
   const handleSearch = (e) => {
     const _searchText = e.target.value;
     setSearchText(_searchText)
   }
+  
   const clickHandler = () => {
     (async () => {
       try{
         const response = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=cda52829&s=${searchText}`)
         if (response.status === 200) {
           const result = await response.json()
-          setData(result)
-          console.log(result)
+          setData(result.Search)
           
         }else {
           throw new Error("Kein data gefunden")
@@ -30,16 +30,23 @@ function App() {
         console.log(error)
       }
     })()
-
   }
-  // const moviesListe = data.map((el)=> <Movies el={el}/>)
+
   return (
     <div className="App">
       <h2>data IMDB</h2>
       <input type="text" value={searchText} onChange={(e) => handleSearch(e)} /><button onClick={clickHandler}>Search</button>
-      {/* <div className='container'>
-        {moviesListe}
-      </div> */}
+      <h2>You found {data.length} movies</h2>
+      {data.length > 0 &&
+        <Row>
+          {data.map((el, index)=> (
+              <Col key={index}>
+                <Movies movieId={el.imdbID} />
+              </Col>
+            )
+          )}
+        </Row>
+      } 
     </div>
     
   );
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,17 +1,29 @@
 import Card from 'react-bootstrap/Card';
+import { useState, useEffect } from "react"
 
-export default function Movies (el) {
-    console.log(el)
-    return(
+export default function Movies ({movieId}) {
+    const [movie, setMovie] = useState({})
+    console.log(movie)
+    useEffect(()=>{
+        fetch(`https://www.omdbapi.com/?apikey=cda52829&i=${movieId}`)
+            .then(res => res.json())
+            .then(json => {
+                if (json.Response === "True")
+                    setMovie(json)
+                else 
+                    setMovie({})
+            })
+    }, [movieId])
+   
+    return (
         <Card className="bg-dark text-white">
-        <Card.Img src="holder.js/100px270" alt="Card image" />
+        <Card.Img src={movie["Poster"]} alt="Card image" />
         <Card.ImgOverlay>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
+          <Card.Title>{movie["Title"]}</Card.Title>
+          <Card.Text>{movie["Released"]}
+            
           </Card.Text>
-          <Card.Text>Last updated 3 mins ago</Card.Text>
+          <Card.Text>{movie["imdbRating"]}/10</Card.Text>
         </Card.ImgOverlay>
       </Card>
         
